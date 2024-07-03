6c0d9c1b0434a1a40b3f3b785afc598876fd81ce
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-07-03 10:32:55 +0900

    #10

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@
 
 import { Links } from "./components/Links";
 import { Headline } from "./components/Headline";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const handleClick = useCallback((e: any) => {
     e.preventDefault();
     alert("ボタンがクリックされました");
   }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    }
+  }, [])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -18,7 +27,10 @@ export default function Home() {
         obj={{ foo: "foo", bar: 15 }}
         bool={false}
       />
-      <button onClick={handleClick}>ボタン</button>
+      <section className="flex gap-3">
+        <button onClick={handleClick}>ボタン</button>
+        <Link href="/about">about</Link>
+      </section>
       <Links />
     </main>
   );
