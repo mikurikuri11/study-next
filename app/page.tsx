38945f8b870ad31206021f7ec56de3258a9812ab
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-07-03 11:38:47 +0900

    #14

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,38 @@ import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
-  const [foo, setFoo] = useState(1);
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e: any) => {
-    console.log(foo);
-    if (foo < 10) {
-      setFoo(foo => foo + 1);
+  const handleClick = useCallback(
+    (e: any) => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
+
+  const handleChange = useCallback((e: any) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内で入力してください");
+      return;
     }
-  }, [foo]);
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleShow = useCallback(() => {
+    setIsShow(isShow => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
       document.body.style.backgroundColor = "";
-    }
-  }, [foo])
+    };
+  }, [count]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -31,9 +47,16 @@ export default function Home() {
         obj={{ foo: "foo", bar: 15 }}
         bool={false}
       />
-      <h1>{foo}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <section className="flex gap-3">
         <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleShow}>真偽値ボタン</button>
+        <input
+          className="bg-gray-500 p-2 rounded-md"
+          type="text"
+          value={text}
+          onChange={handleChange}
+        />
         <Link href="/about">about</Link>
       </section>
       <Links />
